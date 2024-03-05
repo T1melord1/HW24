0d2fdf368aafd7e1ab6275db42891e822984545e
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-03-05 17:19:17 +0300

    Создание метода newName который спрашивает имя человека и если его нет в базе, то просит добавить номер

--- HW24/src/Main.java
+++ HW24/src/Main.java
@@ -1,9 +1,57 @@
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main {
-    Map<String,String> map = new TreeMap<>();
+    private static final Set<String> peopleName = new TreeSet<>();
+    private static final Set<String> peopleNumber = new TreeSet<>();
+    private static final Map<Set<String>, Set<String>> nameNumber = new HashMap<>();
     public static void main(String[] args) {
+        boolean run = true;
+        while (run){
+//        System.out.println("Имя добавленного человека: " + addName());
+//        System.out.println("Номер добавленного человека: " + addNumber());
+        nameNumber.put(peopleName,peopleNumber);
+        newName();
 
+            for (Map.Entry<Set<String>, Set<String>> entry : nameNumber.entrySet()) {
+                String name = "";
+                String number = "";
+                for (String s : entry.getKey()) {
+                    name = s;
+                }
+                for (String s : entry.getValue()) {
+                    number = s;
+                }
+                System.out.println(name + "" + number);
+            }
+
+    }
+    }
+
+    public static String addName(){
+        System.out.println("Введите имя человека: ");
+        String name = new Scanner(System.in).nextLine();
+        peopleName.add(name);
+        return name;
+    }
+
+    public static String addNumber(){
+        System.out.println("Введите номер человека: ");
+        String number = new Scanner(System.in).nextLine();
+        peopleNumber.add(number);
+        return number;
+    }
+    public static void newName(){
+        System.out.println("Введите имя человека: ");
+        String name = new Scanner(System.in).nextLine();
+        if(!(peopleNumber.contains(name))){
+            System.out.println("Такого имени в телефонной книге нет");
+            peopleNumber.add(name);
+            System.out.println("Введите номер телефона для абонента " + name);
+            String number = new Scanner(System.in).nextLine();
+            peopleNumber.add(number);
+            System.out.println("Контакт сохранён!");
+        }else {
+            System.out.println("Такое имя уже есть в базе!");
+        }
     }
 }
