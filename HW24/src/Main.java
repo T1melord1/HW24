79011772ba253fea4011a18f52246cd8d5f59104
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-03-05 17:59:37 +0300

    Условие:
    1. Напишите программу, которая будет работать как телефонная книга:
    ·Если вводим новое имя, программа просит ввести номер телефона и запоминает его.
    ·Если новый номер телефона — просит ввести имя и также запоминает.
    ·Если вводим существующее имя или номер телефона, программа выводит телефон(ы) или имя абонента соответственно.
    ·При вводе команды LIST программа печатает в консоль список всех абонентов в алфавитном порядке с номерами.
    2. Определяйте имя и телефон с помощью регулярных выражений.
    3. Подумайте, что выбрать в качестве ключа и значения для Map, и выберите лучший, по вашему мнению, вариант. Опишите, какие минусы и плюсы видите в своём выборе.
    Примеры работы с телефонной книгой (жирным шрифтом выделен ввод пользователя)
    Введите номер, имя или команду:
    Маша
    Такого имени в телефонной книге нет.
    Введите номер телефона для абонента “Маша”:
    79001234567
    Контакт сохранен!
    
    Введите номер, имя или команду:
    79007654321
    Такого номера нет в телефонной книге.
    Введите имя абонента для номера “79007654321”:
    Маша
    Контакт сохранен!
    
    Введите номер, имя или команду:
    Nfif@
    Неверный формат ввода
    
    Введите номер, имя или команду:
    LIST
    Маша - 79001234567, 79007654321
    
    Подсказки.
    Задание гораздо сложнее всего, что мы разбирали до этого момента.
    Не пытайтесь сделать его за 5 минут.
    Начните с простого: придумайте корректные регулярки которые будут правильно понимать ввод пользователя(имя ли это? Номер? Команда?).
    Протестируйте регулярки(например чтобы при вводе номера программа писала что это номер и т.д.).
    Соберите программу так, чтобы все методы вызывались в нужное время и в нужном месте. Добавьте необходимые проверки и условия. Протестируйте работу программы.

--- HW24/src/Main.java
+++ HW24/src/Main.java
@@ -4,29 +4,29 @@ public class Main {
     private static final Set<String> peopleName = new TreeSet<>();
     private static final Set<String> peopleNumber = new TreeSet<>();
     private static final Map<Set<String>, Set<String>> nameNumber = new HashMap<>();
+    private static final String correctNumber = "\\D";
     public static void main(String[] args) {
         boolean run = true;
         while (run){
 //        System.out.println("Имя добавленного человека: " + addName());
 //        System.out.println("Номер добавленного человека: " + addNumber());
         nameNumber.put(peopleName,peopleNumber);
-        newName();
+        firstName();
 
             for (Map.Entry<Set<String>, Set<String>> entry : nameNumber.entrySet()) {
-                String name = "";
-                String number = "";
-                for (String s : entry.getKey()) {
-                    name = s;
-                }
-                for (String s : entry.getValue()) {
-                    number = s;
-                }
-                System.out.println(name + "" + number);
-            }
-
+                System.out.println(entry.getKey() + "" + entry.getValue());
     }
+        }
     }
-
+    public static void nameOrNumber(){
+        System.out.println("Введите номер или имя человека");
+        String input = new Scanner(System.in).nextLine();
+        if(input.matches(correctNumber)){
+//            Метод для номера
+        }else{
+            firstName();
+        }
+    }
     public static String addName(){
         System.out.println("Введите имя человека: ");
         String name = new Scanner(System.in).nextLine();
@@ -38,16 +38,16 @@ public class Main {
         String number = new Scanner(System.in).nextLine();
         return number;
     }
-    public static void newName(){
+    public static void firstName(){
         String name = addName();
-        if(!(peopleNumber.contains(name))){
-            System.out.println("Такого имени в телефонной книге нет");
-            peopleNumber.add(name);
+        if(!(peopleName.contains(name))){
+            System.out.println("Такого имени в телефонной книге нет и было добавлено");
+            peopleName.add(name);
             String number = addNumber();
             peopleNumber.add(number);
             System.out.println("Контакт сохранён!");
         }else {
             System.out.println("Такое имя уже есть в базе!");
         }
+        }
     }
-}
