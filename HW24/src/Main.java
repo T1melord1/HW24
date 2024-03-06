76f94f9aa3052985cd5bcb8bbfe03b6b843cab47
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-03-06 16:04:31 +0300

    Введите номер или имя человека  Влад Введите номер телефона:  375333070321 Такого номера в телефонной книге нет и был добавлен Контакт сохранён! [Влад][375333070321] Введите номер или имя человека  Олег Введите номер телефона:  375333070321 Такой номер есть в справочнике [Влад, Олег][375333070321] [Влад, Олег][375333070321]

--- HW24/src/Main.java
+++ HW24/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     private static final Set<String> peopleName = new TreeSet<>();
     private static final Set<String> peopleNumber = new TreeSet<>();
     private static final Map<Set<String>, Set<String>> nameNumber = new HashMap<>();
-    private static final String correctNumber = "\\d";
+    private static final String correctNumber = "\\D+";
     public static void main(String[] args) {
         boolean run = true;
         while (run){
@@ -22,11 +22,11 @@ public class Main {
         System.out.println("Введите номер или имя человека");
         String input = scanner();
         if(input.matches(correctNumber)){
-        firstNumber();
-        firstName();
-        }else{
-            firstName();
+        peopleName.add(input);
             firstNumber();
+        }else{
+            peopleNumber.add(input);
+            firstName();
         }
     }
     public static String addName(){
@@ -39,28 +39,31 @@ public class Main {
     public static String scanner(){
         return new Scanner(System.in).nextLine();
     }
-    public static void firstName(){
-        if(!(peopleName.contains(name))){
-            System.out.println("Такого имени в телефонной книге нет и было добавлено");
-            peopleName.add(name);
-            String number = addNumber();
-            peopleNumber.add(number);
-            System.out.println("Контакт сохранён!");
-        }else {
-            System.out.println("Такое имя уже есть в базе!");
-        }
-        }
-
-        public static void firstNumber(){
+    public static void firstNumber(){
+        System.out.println("Введите номер телефона:");
+        String number = addNumber();
         if(!(peopleNumber.contains(number))){
             System.out.println("Такого номера в телефонной книге нет и был добавлен");
             peopleNumber.add(number);
-            String name = addName();
+            System.out.println("Контакт сохранён!");
+        }
+        else
+        {
+            System.out.println("Такой номер есть в справочнике");
+        }
+    }
+
+    public static void firstName(){
+        System.out.println("Введите имя человека");
+        String name = addName();
+        if(!(peopleName.contains(name))){
+            System.out.println("Такого имени в телефонной книге нет и было добавлено");
             peopleName.add(name);
             System.out.println("Контакт сохранён!");
         }
-        else {
-            System.out.println("Такой номер уже есть в базе!");
+        else
+        {
+            System.out.println("Такое имя есть в справочнике");
         }
         }
 }
